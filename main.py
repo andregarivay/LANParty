ddae0227f2a2399803a1ab0ee3d73da37b4ed9e9
Andre Garivay <andregarivay@gmail.com>
2017-07-24 10:41:28 -0700

    Added login portal

--- main.py
+++ main.py
@@ -12,9 +12,27 @@ jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template('main.html')
+        cur_user = users.get_current_user()
+        log_url = ''
+        if not cur_user:
+            template = jinja_env.get_template('main.html')
+            self.response.out.write(template.render())
+        else:
+            template = jinja_env.get_template('profile.html')
+            self.response.out.write(template.render())
+
+class Signup(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('signup.html')
         self.response.out.write(template.render())
 
+class Login(webapp2.RequestHandler):
+    def get(self):
+        log_url = users.create_login_url('/')
+        self.redirect(log_url)
+
 app = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage),
+    ('/signup', Signup),
+    ('/login', Login),
 ], debug= True)
