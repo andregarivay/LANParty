c0c799601d034e7b49a5b26fab633dda9fd95e08
Leonel Chacon <leonel.chacon99@gmail.com>
2017-07-24 11:20:47 -0700

    class ChatHandler

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import jinja2
+import random
 import logging
 import os
 import json
@@ -9,14 +10,6 @@ from google.appengine.api import users
 from google.appengine.ext import ndb
 
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
-
-class User(ndb.Model):
-    first_name = ndb.StringProperty()
-    last_name = ndb.StringProperty()
-    job = ndb.StringProperty()
-    city = ndb.StringProperty()
-    state = ndb.StringProperty()
-    bio = ndb.StringProperty()
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -42,6 +35,11 @@ class Login(webapp2.RequestHandler):
     def get(self):
         log_url = users.create_login_url('/')
         self.redirect(log_url)
+
+class ChatHandler(webapp2.RequestHandler):
+    def get(self):
+        if cur_user:
+            unique_user_id = random.randint(0, 1000000)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
