78c1eace37daa8f2ba713c8838adb6cdc33b7d79
Andre Garivay <andregarivay@gmail.com>
2017-07-24 11:35:47 -0700

    Created many handlers

--- main.py
+++ main.py
@@ -17,25 +17,33 @@ class User(ndb.Model):
     city = ndb.StringProperty()
     state = ndb.StringProperty()
     bio = ndb.StringProperty()
+    email = ndb.StringProperty(indexed = True)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        cur_user = users.get_current_user()
         log_url = ''
-        if not cur_user:
-            template = jinja_env.get_template('main.html')
-            self.response.out.write(template.render())
-        else:
-            log_url = users.create_logout_url('/')
-            variables = {
-                'log_url': log_url
-            }
-            template = jinja_env.get_template('profile.html')
-            self.response.out.write(template.render(variables))
+        template = jinja_env.get_template('templates/main.html')
+        self.response.out.write(template.render())
+
+class Help(webapp2.RequestHandler):
+    def get(self):
+        cur_user = users.get_current_user()
+        email = cur_user.email()
+        if email:
+            key = ndb.Key('User', email)
+            user_email = key.get()
+            if not user_email:
+                self.redirect('/signup')
+        log_url = users.create_logout_url('/')
+        variables = {
+            'log_url': log_url
+        }
+        template = jinja_env.get_template('templates/profile.html')
+        self.response.out.write(template.render(variables))
 
 class Signup(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template('signup.html')
+        template = jinja_env.get_template('templates/signup.html')
         self.response.out.write(template.render())
 
 class Login(webapp2.RequestHandler):
@@ -43,8 +51,15 @@ class Login(webapp2.RequestHandler):
         log_url = users.create_login_url('/')
         self.redirect(log_url)
 
+class Profile(webapp2.RequestHandler):
+    def post(self):
+        template = jinja_env.get_template('templates/profile.html')
+        self.response.out.write(template.render())
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/start', Help),
     ('/signup', Signup),
     ('/login', Login),
+    ('/profile', Profile)
 ], debug= True)
